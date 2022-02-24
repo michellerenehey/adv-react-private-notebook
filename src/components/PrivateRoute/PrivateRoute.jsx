fd3fcd811b5b5711aaf53526eb260621b492ed6f
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-02-24 10:41:42 -0800

    pushing for redeploy

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,10 +1,7 @@
 import { Redirect, Route } from 'react-router-dom';
 import { useUser } from '../../context/UserContext';
 
-// A wrapper for <Route> that redirects to the login
-// screen if you're not yet authenticated.
 export default function PrivateRoute({ children, ...rest }) {
-  // TODO: Use the user in context to determine whether to redirect to /login
   const { user } = useUser();
 
   return (
